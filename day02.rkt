511752f7cee6a294a7e2ccded568c3c6b84050ad
Bogdan Popa <bogdan@defn.io>
2023-12-02 10:25:03 +0200

    add day 2

---
+++ day02.rkt
@@ -0,0 +1,63 @@
+#lang racket/base
+
+(require racket/match
+         racket/string)
+
+(struct game (id sets)
+  #:transparent)
+
+(define (parse-game line)
+  (match-define (regexp #rx"Game ([^:]+): (.+)"
+                        (list _ (app string->number id) sets-str))
+    line)
+  (define sets
+    (for/list ([set-str (in-list (string-split sets-str ";"))])
+      (for/hasheq ([reveal-str (in-list (string-split set-str ","))])
+        (match-define (regexp #rx"([0-9]+) ([a-z]+)"
+                              (list _
+                                    (app string->number n)
+                                    (app string->symbol color)))
+          reveal-str)
+        (values color n))))
+  (game id sets))
+
+(define (game-ok? g proc)
+  (for/and ([s (in-list (game-sets g))])
+    (proc s)))
+
+(define (part1-ok? s)
+  (and
+   (<= (hash-ref s 'red 0) 12)
+   (<= (hash-ref s 'green 0) 13)
+   (<= (hash-ref s 'blue 0) 14)))
+
+(define (game-power g)
+  (define minimums
+    (for/fold ([minimums (hasheq 'red 0 'green 0 'blue 0)])
+              ([s (in-list (game-sets g))])
+      (for/fold ([minimums minimums])
+                ([color (in-list '(red green blue))])
+        (hash-update minimums color (λ (n) (max n (hash-ref s color 0)))))))
+  (apply * (hash-values minimums)))
+
+(define part1
+  (call-with-input-file "day02.txt"
+    (lambda (in)
+      (for*/sum ([line (in-lines in)]
+                 [game (in-value (parse-game line))]
+                 #:when (game-ok? game part1-ok?))
+        (game-id game)))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? part1 2771))
+
+(define part2
+  (call-with-input-file "day02.txt"
+    (lambda (in)
+      (for*/sum ([line (in-lines in)]
+                 [game (in-value (parse-game line))])
+        (game-power game)))))
+
+(module+ test
+  (check-equal? part2 70924))
