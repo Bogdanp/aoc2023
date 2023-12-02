f49c572683e52bc8904e7aa033dae37b484925a5
Bogdan Popa <bogdan@defn.io>
2023-12-02 11:03:00 +0200

    extract parse-set and game-minimums

--- day02.rkt
+++ day02.rkt
@@ -6,19 +6,21 @@
 (struct game (id sets)
   #:transparent)
 
+(define (parse-set set-str)
+  (for/hasheq ([reveal-str (in-list (string-split set-str ","))])
+    (match-define (regexp #rx"([0-9]+) ([a-z]+)"
+                          (list _
+                                (app string->number n)
+                                (app string->symbol color)))
+      reveal-str)
+    (values color n)))
+
 (define (parse-game line)
   (match-define (regexp #rx"Game ([^:]+): (.+)"
                         (list _ (app string->number id) sets-str))
     line)
   (define sets
-    (for/list ([set-str (in-list (string-split sets-str ";"))])
-      (for/hasheq ([reveal-str (in-list (string-split set-str ","))])
-        (match-define (regexp #rx"([0-9]+) ([a-z]+)"
-                              (list _
-                                    (app string->number n)
-                                    (app string->symbol color)))
-          reveal-str)
-        (values color n))))
+    (map parse-set (string-split sets-str ";")))
   (game id sets))
 
 (define (game-ok? g proc)
@@ -31,14 +33,15 @@
    (<= (hash-ref s 'green 0) 13)
    (<= (hash-ref s 'blue 0) 14)))
 
+(define (game-minimums g)
+  (for/fold ([minimums (hasheq 'red 0 'green 0 'blue 0)])
+            ([s (in-list (game-sets g))])
+    (for/fold ([minimums minimums])
+              ([color (in-list '(red green blue))])
+      (hash-update minimums color (λ (n) (max n (hash-ref s color 0)))))))
+
 (define (game-power g)
-  (define minimums
-    (for/fold ([minimums (hasheq 'red 0 'green 0 'blue 0)])
-              ([s (in-list (game-sets g))])
-      (for/fold ([minimums minimums])
-                ([color (in-list '(red green blue))])
-        (hash-update minimums color (λ (n) (max n (hash-ref s color 0)))))))
-  (apply * (hash-values minimums)))
+  (apply * (hash-values (game-minimums g))))
 
 (define part1
   (call-with-input-file "day02.txt"
